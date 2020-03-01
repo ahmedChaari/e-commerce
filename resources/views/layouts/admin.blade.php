32d23c293019be9ea55d66784f78bd5af46ba11b
ahmedChaari <ahmed17ch@gmail.com>
2020-03-01 23:12:27 +0100

    Modif-login-page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,6 +10,7 @@
 
     Travel Company Manage
 </title>
+
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
